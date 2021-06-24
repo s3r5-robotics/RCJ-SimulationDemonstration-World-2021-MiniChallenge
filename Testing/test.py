caf604c91aec14adf82e547440fb7cf07a4f3f7f
Gasper <gaspercino.lukman@gmail.com>
2021-06-24 18:44:04 +0200

    Moved
    Changed to gyro.

--- Testing/test.py
+++ Testing/test.py
@@ -9,18 +9,19 @@ class GPS:
         self.x = 0
         self.z = 0
 
-    def update(self):
+    # pass in the current wheels velocity.
+    def update(self, velocity):
         timeElapsed = time - self.oldTime  # Time passed in time step
         radsInTimestep = (self.sensor.getValues())[self.index] * timeElapsed
         self.lastRads = radsInTimestep
         finalRot = self.rotation + radsInTimestep
         self.rotation = normalizeRads(finalRot)
         self.oldTime = time
-        self.x, self.z = getXY(self.sensor.getValues()[self.index], radsToDegs(self.rotation))
+        self.x, self.z = getXY(velocity, radsToDegs(self.rotation))
 
 
 # Getting components of vector
 def getXY(magnitude, angle):
-    x = cos(angle) * magnitude
-    y = sin(angle) * magnitude
+    x = cos(angle) * magnitude # get x component
+    y = sin(angle) * magnitude # get y component
     return x, y
